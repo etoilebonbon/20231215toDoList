4e5b00db0e6f178c894fd30734a790437028fadb
etoilebonbon <violetjardin@daum.net>
2023-12-15 10:25:47 +0900

    delete option

--- toDoList.js
+++ toDoList.js
@@ -13,10 +13,13 @@ function deleteToDo(e) { //e = event
     // alert("삭제");
     const li = e.target.parentElement
     li.remove()
+    toDos = toDos.filter((item) => item.id !== parseInt(li.id))
+    saveToDos()
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.setAttribute("id",newTodo.id)
     const span = document.createElement("span");
     const button = document.createElement("button");
 
@@ -24,22 +27,25 @@ function paintToDo(newTodo) {
     button.addEventListener("click", deleteToDo)
     li.appendChild(span)
     li.appendChild(button)
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     toDoList.appendChild(li)
 }
 
 function handleToDoSubmit (event) {
     event.preventDefault(); //새로고침 막음
     // console.log(toDoInput.value)
-    const newTodo = toDoInput.value;
+    const newTodoObj = {
+        text: toDoInput.value,
+        id: Date.now()
+    }
 
     toDoInput.value = "";
 
     // 그려주는 함수
-    paintToDo(newTodo)
+    paintToDo(newTodoObj)
     
     //로컬스토리지에 저장하는 함수
-    toDos.push(newTodo)
+    toDos.push(newTodoObj)
     saveToDos();
 } 
 
